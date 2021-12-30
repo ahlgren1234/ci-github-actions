8688d1a62452b5911de26996779196c41d80c760
Peter Ahlgren <ahlgren1234@gmail.com>
2021-12-30 14:11:58 +0100

    run linting on new test

--- attack.test.js
+++ attack.test.js
@@ -4,5 +4,5 @@ test('should return damage sentence when called correctly', () => {
   const result = attack('Smelly Ogre', 23);
 
   expect(result).toMatch(/^Smelly Ogre(.*)23/);
-  expect(result).toMatchInlineSnapshot(`"Smelly Ogre dealt 23 damage!"`);
+  expect(result).toMatchInlineSnapshot('"Smelly Ogre dealt 23 damage!"');
 });
